5b2953e0d8fee6af6f61eece4ef90f07f8ce1d77
artem256code <artem_code@outlook.com>
2021-01-21 12:52:53 +0300

    Добавлен скрипт строящий диаграммы

---
+++ graphics.py
@@ -0,0 +1,47 @@
+from matplotlib import pyplot
+
+def parse(path):
+    x = []
+    y = []
+    file = open(path, "r")
+    for line in file:
+        line = line.replace("\n", "") 
+        x.append(int(line.split(" ")[0]))
+        y.append(int(line.split(" ")[1]))
+    return x,y
+
+
+# Значения до симуляции
+x1, y1 = parse("input.txt")     
+x3 = x1
+y3 = [y1[i] * x1[i] for i in range(len(y1))]   
+
+# Значения после симуляции
+x2, y2 = parse("out.txt")       
+x4 = x2
+y4 = [y2[i] * x2[i] for i in range(len(y2))]
+
+
+pyplot.subplot(2, 2, 1)
+pyplot.bar(x1, y1)
+pyplot.title('До симуляции')
+pyplot.xlabel('Масса фракции')
+pyplot.ylabel('Кол-во кластеров')
+
+pyplot.subplot(2, 2, 2)
+pyplot.bar(x2, y2)
+pyplot.title('После симуляции')
+pyplot.xlabel('Масса фракции')
+pyplot.ylabel('Кол-во кластеров')
+
+pyplot.subplot(2, 2, 3)
+pyplot.bar(x3, y3)
+pyplot.xlabel('Масса фракции')
+pyplot.ylabel('Объём фракции')
+
+pyplot.subplot(2, 2, 4)
+pyplot.bar(x4, y4)
+pyplot.xlabel('Масса фракции')
+pyplot.ylabel('Объём фракции')
+
+pyplot.show()
